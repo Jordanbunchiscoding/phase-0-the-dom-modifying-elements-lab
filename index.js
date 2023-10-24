828a0f2cc3c92730d89685ab4d21b5011dc57efb
jordan <jordanbunch9@gmail.com>
2023-10-24 10:01:20 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Write your code here!
+const newHeader = document.createElement('h1');
+newHeader.id = 'victory';
+newHeader.textContent = 'YOUR-NAME is the champion';
+document.body.appendChild(newHeader);
+
+const mainElement = document.querySelector('main');
+mainElement.remove();
+
+function runTests() {
+  describe("index.html", function() {
+    // Your existing test code here
+  });
+}
+
+runTests();
